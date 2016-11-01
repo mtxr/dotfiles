959f12bd8dc0f9a8e78bee5f46752b03df1d7af5
Matheus Teixeira <matheus.mtxr@gmail.com>
2016-11-01 21:39:07 -0200

    ter nov  1 21:39:07 BRST 2016

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -3,4 +3,4 @@
 # zsh-completions
 fpath=(~/.zsh/plugins/zsh-completions/src $fpath)
 
-source /.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
