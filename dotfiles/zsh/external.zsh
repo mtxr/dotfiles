2fcc1debdb56349a2931966e9b8bf9ebc33a56df
Matheus Teixeira <matheus.mtxr@gmail.com>
2016-11-03 20:27:22 -0200

    qui nov  3 20:27:22 BRST 2016

--- dotfiles/zsh/external.zsh
+++ dotfiles/zsh/external.zsh
@@ -1,5 +1,5 @@
 # pip should only run if there is a virtualenv currently activated
-export PIP_REQUIRE_VIRTUALENV=true
+export PIP_REQUIRE_VIRTUALENV=false
 
 # Cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
