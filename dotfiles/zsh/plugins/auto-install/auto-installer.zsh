e112b4ebe399921e3e948a19bceb4f203c4c285d
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-04-12 13:33:51 -0300

    Wed Apr 12 13:33:51 BRT 2017

--- dotfiles/zsh/plugins/auto-install/auto-installer.zsh
+++ dotfiles/zsh/plugins/auto-install/auto-installer.zsh
@@ -2,6 +2,11 @@ export N_PREFIX=$([ "$N_PREFIX" = "" ] && echo $HOME/n || echo $N_PREFIX)
 
 if [[ ! -e $N_PREFIX ]]; then
     echo 'Will install N, the node version manager...'
-    INSTALLER=$(hash wget &> /dev/null && echo "wget -qO-" || echo "curl -L")
+    INSTALLER=$(type wget &> /dev/null && echo "wget -qO-" || echo "curl -L")
     eval $INSTALLER https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -y -n
+fi
+
+if [[ "$(type diff-so-fancy &> /dev/null && echo $?)" = "1" ]]; then
+    echo "Installing 'diff-so-fancy'..."
+    npm install -g diff-so-fancy &> /dev/null && echo "Installed!" || echo "##### Failed to install 'diff-so-fancy'."
 fi
