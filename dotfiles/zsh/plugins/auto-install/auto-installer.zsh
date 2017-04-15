9ddc4cc95df24ce22a9f1d6a5e423e64768d2ce7
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-04-14 21:19:54 -0300

    Fri Apr 14 21:19:54 BRT 2017

--- dotfiles/zsh/plugins/auto-install/auto-installer.zsh
+++ dotfiles/zsh/plugins/auto-install/auto-installer.zsh
@@ -6,7 +6,12 @@ if [[ ! -e $N_PREFIX ]]; then
     eval $INSTALLER https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -y -n
 fi
 
+if [[ "$(type yarn &> /dev/null && echo $?)" = "1" ]]; then
+    echo "Installing 'yarn'..."
+    npm install -g yarn &> /dev/null && echo "Installed!" || echo "##### Failed to install 'yarn'."
+fi
+
 if [[ "$(type diff-so-fancy &> /dev/null && echo $?)" = "1" ]]; then
     echo "Installing 'diff-so-fancy'..."
-    npm install -g diff-so-fancy &> /dev/null && echo "Installed!" || echo "##### Failed to install 'diff-so-fancy'."
+    yarn global add diff-so-fancy &> /dev/null && echo "Installed!" || echo "##### Failed to install 'diff-so-fancy'."
 fi
