f44a0c0dba179cca1d66ceedd44d5a8d5d63b91f
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-05-19 16:29:32 -0300

    Fri May 19 16:29:32 -03 2017

--- dotfiles/zsh/plugins/auto-install/auto-installer.zsh
+++ dotfiles/zsh/plugins/auto-install/auto-installer.zsh
@@ -1,17 +1,27 @@
+export WEB_INSTALLER=$(type wget &> /dev/null && echo "wget -qO-" || echo "curl -L")
 export N_PREFIX=$([ "$N_PREFIX" = "" ] && echo $HOME/n || echo $N_PREFIX)
+
+type colordiff &> /dev/null
+if [[ "$?" = "1" ]]; then
+    echo "Installing 'colordiff'..."
+    COLORDIFF_BIN_LOCATION=${COLORDIFF_BIN_LOCATION:-"/usr/local/bin/colordiff"}
+    eval sudo $WEB_INSTALLER https://raw.githubusercontent.com/daveewart/colordiff/current/colordiff.pl > $COLORDIFF_BIN_LOCATION && sudo chmod +x $COLORDIFF_BIN_LOCATION && echo "Installed!" || echo "##### Failed to install 'colordiff'."
+fi
 
 if [[ ! -e $N_PREFIX ]]; then
     echo 'Will install N, the node version manager...'
-    INSTALLER=$(type wget &> /dev/null && echo "wget -qO-" || echo "curl -L")
-    eval $INSTALLER https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -y -n
+    eval $WEB_INSTALLER https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -y -n && echo "Installed!" || echo "##### Failed to install 'N'."
 fi
 
-if [[ "$(type yarn &> /dev/null && echo $?)" = "1" ]]; then
+type yarn &> /dev/null
+if [[ "$?" = "1" ]]; then
     echo "Installing 'yarn'..."
     npm install -g yarn &> /dev/null && echo "Installed!" || echo "##### Failed to install 'yarn'."
 fi
 
-if [[ "$(type diff-so-fancy &> /dev/null && echo $?)" = "1" ]]; then
+type diff-so-fancy &> /dev/null
+if [[ "$?" = "1" ]]; then
     echo "Installing 'diff-so-fancy'..."
     yarn global add diff-so-fancy &> /dev/null && echo "Installed!" || echo "##### Failed to install 'diff-so-fancy'."
 fi
+
