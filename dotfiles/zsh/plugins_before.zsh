02ee2445e6a4a4da5a1128aaffdbc2a3798e3e3d
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-05-25 18:45:36 -0300

    Thu May 25 18:45:36 -03 2017

--- dotfiles/zsh/plugins_before.zsh
+++ dotfiles/zsh/plugins_before.zsh
@@ -2,7 +2,7 @@
 
 # Check auto installs
 . $HOME/.zsh/plugins/auto-install/auto-installer.zsh
-. $HOME/.zsh/plugins/lwd/bin/lwd.sh
+. $HOME/.zsh/plugins/lwd/lwd.sh
 
 # zsh-completions
 fpath=($HOME/.zsh/plugins/zsh-completions/src $fpath)
