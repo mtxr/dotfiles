67773b06cc463612b56a7494b26ad6dd0b1ff745
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-05-25 11:16:29 -0300

    Thu May 25 11:16:29 -03 2017

--- dotfiles/zsh/plugins/auto-install/auto-installer.zsh
+++ dotfiles/zsh/plugins/auto-install/auto-installer.zsh
@@ -1,13 +1,6 @@
 export WEB_INSTALLER=$(type wget &> /dev/null && echo "wget -qO-" || echo "curl -L")
 export N_PREFIX=$([ "$N_PREFIX" = "" ] && echo $HOME/n || echo $N_PREFIX)
 
-type colordiff &> /dev/null
-if [[ "$?" = "1" ]]; then
-    echo "Installing 'colordiff'..."
-    COLORDIFF_BIN_LOCATION=${COLORDIFF_BIN_LOCATION:-"/usr/local/bin/colordiff"}
-    eval sudo $WEB_INSTALLER https://raw.githubusercontent.com/daveewart/colordiff/current/colordiff.pl > $COLORDIFF_BIN_LOCATION && sudo chmod +x $COLORDIFF_BIN_LOCATION && echo "Installed!" || echo "##### Failed to install 'colordiff'."
-fi
-
 if [[ ! -e $N_PREFIX ]]; then
     echo 'Will install N, the node version manager...'
     eval $WEB_INSTALLER https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -y -n && echo "Installed!" || echo "##### Failed to install 'N'."
