8a88a5dab2e9995d04ad6856a04a9428c5a17c8b
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-08-19 14:38:30 -0300

    Sat Aug 19 14:38:30 -03 2017

--- dotfiles/zsh/functions/docker.zsh
+++ dotfiles/zsh/functions/docker.zsh
@@ -1,12 +1,24 @@
-_fn_docker_option_list() {
+_fn_docker_commands_option_list() {
+  { echo Command$'\t'Description ; (docker 2>&1 | rg '^  ([a-z]+) +(.+)$' --replace "\$1"$'\t'"\$2" --color=never) } | column -t -s $'\t'
+}
+
+_fn_docker_container_option_list() {
   docker ps --format "table  {{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.Image}}" -a
 }
 
 _fzf_complete_docker() {
-  _fzf_complete "$FZF_DEFAULT_OPTS --header-lines=1" "$@" < <(
-    _fn_docker_option_list
-  )
-  # zle accept-line
+  ARGS="$@"
+  if [[ "$ARGS" =~ '^ *docker +(start|restart|stop) *$' ]];then
+    _fzf_complete "$FZF_DEFAULT_OPTS --header-lines=1" "$@" < <(
+      _fn_docker_container_option_list
+    )
+  elif [[ "$ARGS" =~ '^ *docker *$' ]];then
+    _fzf_complete "$FZF_DEFAULT_OPTS --header-lines=1" "$@" < <(
+      _fn_docker_commands_option_list
+    )
+  else
+    eval "zle ${fzf_default_completion:-expand-or-complete}"
+  fi
 }
 
 _fzf_complete_docker_post() {
