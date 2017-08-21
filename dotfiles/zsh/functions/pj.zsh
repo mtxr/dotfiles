df011deb6271b966f6e6bf7810d464edc68cf3dc
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-08-20 22:05:27 -0300

    Sun Aug 20 22:05:27 -03 2017

--- dotfiles/zsh/functions/pj.zsh
+++ dotfiles/zsh/functions/pj.zsh
@@ -3,19 +3,11 @@ pj () {
 }
 
 _fn_pj_option_list() {
-  local dir max
-  dir="$@"
-  max=1
+  local max=2
   local IFS=$'\n'
-  if [ "$dir" != "" ] && [ ! -d "$PROJECTS/$dir/" ]; then
-    dir=$(dirname $PROJECTS/$dir | rg "(${PROJECTS})/?(.+)" --replace '$2')
-  fi
-  dir="$([ "$dir" != "" ] && echo "$dir" | rg '(.+)/?$' --replace '$1' || echo "")"
-  [ "$dir" = "" ] && max=2
-
-  echo "PROJECT"
-  find $PROJECTS/$dir -maxdepth $max -type d -exec bash -c 'printf "%q\n" "$@"' printf {} ';' | \
-    rg "${PROJECTS}/?${dir}/?(.+)" --replace '$1'
+  echo "Project"
+  find $PROJECTS/ -maxdepth $max -type d -exec bash -c 'printf "%q\n" "$@"' printf {} ';' | \
+    rg "^${PROJECTS}/*(.+)$" --replace '$1'
 }
 
 _fzf_complete_pj() {
