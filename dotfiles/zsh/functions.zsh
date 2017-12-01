713f1c5fe1acf72d8d68b5d5a8452487b9cf5347
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-12-01 00:54:16 -0200

    Fri Dec  1 00:54:16 -02 2017

--- dotfiles/zsh/functions.zsh
+++ dotfiles/zsh/functions.zsh
@@ -3,41 +3,39 @@ function join { local IFS="$1"; shift; echo "$*"; }
 
 # Update workstation
 function wup() {
-    (
-        git -C ~/.workstation remote add updates https://github.com/mtxr/dotfiles.git &> /dev/null
-        git -C ~/.workstation stash &> /dev/null
-        git -C ~/.workstation pull --rebase --stat updates "$(git -C ~/.workstation rev-parse --abbrev-ref HEAD)"
-        git -C ~/.workstation stash apply > /dev/null
-        ~/.workstation/install -q
-    )
+  (
+    git -C ~/.workstation remote add updates https://github.com/mtxr/dotfiles.git &> /dev/null
+    git -C ~/.workstation stash &> /dev/null
+    git -C ~/.workstation pull --rebase --stat updates "$(git -C ~/.workstation rev-parse --abbrev-ref HEAD)"
+    git -C ~/.workstation stash apply > /dev/null
+    ~/.workstation/install -q
+  )
 }
 
 # Save workstation
 function wsv() {
-    (
-        git -C ~/.workstation add . && git -C ~/.workstation commit -m "`date`" && git -C ~/.workstation push
-    )
+  (
+    git -C ~/.workstation add . && \
+    git -C ~/.workstation commit -m "`date`" && \
+    git -C ~/.workstation push
+  )
 }
 
 function wdf() {
-    (
-        git -C ~/.workstation diff
-    )
+  ( git -C ~/.workstation diff )
 }
 
 function wst() {
-    (
-        git -C ~/.workstation status
-    )
+  git -C ~/.workstation status -s
 }
 
 # Use pip without requiring virtualenv
 function syspip() {
-    PIP_REQUIRE_VIRTUALENV="" pip "$@"
+  PIP_REQUIRE_VIRTUALENV="" pip "$@"
 }
 
 function syspip3() {
-    PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
+  PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
 }
 
 # cd to git root directory
@@ -45,95 +43,93 @@ alias cdgr='cd "$(git root)"'
 
 # Create a directory and cd into it
 function md() {
-    mkdir "${1}" && cd "${1}"
+  mkdir "${1}" && cd "${1}"
 }
 
 # Jump to directory containing file
 function jump() {
-    cd "$(dirname ${1})"
+  cd "$(dirname ${1})"
 }
 
 # builtin cd replacement for screen to track cwd (like tmux)
 function scr_cd()
 {
-    cd $1
-    screen -X chdir $PWD
+  cd $1
+  screen -X chdir $PWD
 }
 
 if [[ -n $STY ]]; then
-    alias cd=scr_cd
+  alias cd=scr_cd
 fi
 
 # Go up [n] directories
 function up()
 {
-    local cdir="$(pwd)"
-    if [[ "${1}" == "" ]]; then
-        cdir="$(dirname "${cdir}")"
-    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
-        echo "Error: argument must be a number"
-    elif ! [[ "${1}" -gt "0" ]]; then
-        echo "Error: argument must be positive"
-    else
-        for i in {1..${1}}; do
-            local ncdir="$(dirname "${cdir}")"
-            if [[ "${cdir}" == "${ncdir}" ]]; then
-                break
-            else
-                cdir="${ncdir}"
-            fi
-        done
-    fi
-    cd "${cdir}"
+  local cdir="$(pwd)"
+  if [[ "${1}" == "" ]]; then
+    cdir="$(dirname "${cdir}")"
+  elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+    echo "Error: argument must be a number"
+  elif ! [[ "${1}" -gt "0" ]]; then
+    echo "Error: argument must be positive"
+  else
+    for i in {1..${1}}; do
+      local ncdir="$(dirname "${cdir}")"
+      if [[ "${cdir}" == "${ncdir}" ]]; then
+        break
+      else
+        cdir="${ncdir}"
+      fi
+    done
+  fi
+  cd "${cdir}"
 }
 
 # Execute a command in a specific directory
 function in() {
-    (
-        cd ${1} && shift && ${@}
-    )
+  ( cd ${1} && shift && ${@} )
 }
 
 # Check if a file contains non-ascii characters
 function nonascii() {
-    LC_ALL=C grep -n '[^[:print:][:space:]]' ${1}
+  LC_ALL=C grep -n '[^[:print:][:space:]]' ${1}
 }
 
 # Fetch pull request
 
 function fpr() {
-    if [ "$#" -eq 2 ]; then
-        local repo="${PWD##*/}"
-        local user="${1}"
-        local branch="${2}"
-    elif [ "$#" -eq 3 ]; then
-        local repo="${1}"
-        local user="${2}"
-        local branch="${3}"
-    else
-        echo "Usage: fpr [repo] username branch"
-        return 1
-    fi
+  if [ "$#" -eq 2 ]; then
+    local repo="${PWD##*/}"
+    local user="${1}"
+    local branch="${2}"
+  elif [ "$#" -eq 3 ]; then
+    local repo="${1}"
+    local user="${2}"
+    local branch="${3}"
+  else
+    echo "Usage: fpr [repo] username branch"
+    return 1
+  fi
 
-    git fetch "git@github.com:${user}/${repo}" "${branch}:${user}/${branch}"
+  git fetch "git@github.com:${user}/${repo}" "${branch}:${user}/${branch}"
 }
 
 # Serve current directory
 
 function serve() {
-    pushd ${1:$PWD}; python -m SimpleHTTPServer "${2:-8080}"; popd
+  pushd ${1:$PWD}; python -m SimpleHTTPServer "${2:-8080}"; popd
 }
 
 
 # git functions
 function gnb() {
-    BRANCH_NAME=$1
-    if [ "$#" = 2 ]; then
-        ORIGIN_BRANCH=$1
-        BRANCH_NAME=$2
-        git checkout "$ORIGIN_BRANCH"
-    fi
-    git checkout -b "$BRANCH_NAME"
+  BRANCH_NAME=$1
+  if [ "$#" = 2 ]; then
+    ORIGIN_BRANCH=$1
+    BRANCH_NAME=$2
+    git checkout "$ORIGIN_BRANCH"
+  fi
+  git checkout -b "$BRANCH_NAME"
 }
 
 # ------------------------------------
@@ -157,13 +153,13 @@ function dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"|
 
 # Bash into running container
 function dbash() {
-    if [ "$1" = "" ];then
-        echo "Missing container name." && return 1
-    fi
-    CONTAINER=$(docker ps -n1 --format "{{.Names}}" -aqf "name=$1")
-    echo "Found container: $CONTAINER"
-    shift
-    docker exec -u ${DUS:-'root'} -i -t $CONTAINER ${DSH:-'bash'} $@
+  if [ "$1" = "" ];then
+    echo "Missing container name." && return 1
+  fi
+  CONTAINER=$(docker ps -n1 --format "{{.Names}}" -aqf "name=$1")
+  echo "Found container: $CONTAINER"
+  shift
+  docker exec -u ${DUS:-'root'} -i -t $CONTAINER ${DSH:-'bash'} $@
 }
 
 alias dsh='DSH=sh dbash'
