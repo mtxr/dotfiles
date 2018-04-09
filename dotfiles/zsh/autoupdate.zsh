eb4a48b4ec280aa5483d6a13fe9194e82ce54ea4
Matheus Teixeira <matheus.mtxr@gmail.com>
2018-04-09 10:48:01 -0300

    Seg Abr 9 10:47:59 -03 2018

--- dotfiles/zsh/autoupdate.zsh
+++ dotfiles/zsh/autoupdate.zsh
@@ -3,7 +3,7 @@ if [ "$WORKSTATION_AUTOUPDATE" = "true" ]; then
   local WORKSTATION_LOCKFILE='/var/lock/wup.lock'
   local WORKSTATION_LOGFILE='/tmp/wup.log'
 
-  if [ ! -f "WORKSTATION_LOCKFILE" ] && (
+  if [ ! -f "$WORKSTATION_LOCKFILE" ] && (
     [ ! -f "$WORKSTATION_LOGFILE" ] ||
     [ "$(( (`date +%s` - `date -r $WORKSTATION_LOGFILE +%s`) / 86400 ))" -gt 1 ]
   ); then
