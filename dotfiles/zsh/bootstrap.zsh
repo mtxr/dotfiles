571aba5ec392c3635222fb5b733fc16c9a5f95ab
Matheus Teixeira <matheus.mtxr@gmail.com>
2018-06-24 22:47:47 -0300

    Dom Jun 24 22:47:44 -03 2018

--- dotfiles/zsh/bootstrap.zsh
+++ dotfiles/zsh/bootstrap.zsh
@@ -109,7 +109,6 @@ export FZF_DEFAULT_OPTS="
   --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
   --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
 "
-
 }
 
 _gen_fzf_default_opts $FZF_DEFAULT_OPTS
--- dotfiles/zsh/env.zsh
+++ dotfiles/zsh/env.zsh
@@ -13,3 +13,5 @@ export GIT_CLEAR_BRANCH_EXCLUDE="release|develop|master"
 export fpath=( $HOME/.autoload-zsh $fpath )
 export VSCODE_FILE=$HOME/.vscode-cli
 export GPG_TTY=$(tty)
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig/:/usr/share/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH"
