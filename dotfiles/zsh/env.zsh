fb055cc01fead636e78f61758889a148f2de8220
Matheus Teixeira <matheus.mtxr@gmail.com>
2018-10-03 19:35:14 -0300

    Wed Oct 3 19:35:11 -03 2018

--- dotfiles/zsh/env.zsh
+++ dotfiles/zsh/env.zsh
@@ -15,3 +15,5 @@ export VSCODE_FILE=$HOME/.vscode-cli
 export GPG_TTY=$(tty)
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig/:/usr/share/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+eval `gnome-keyring-daemon --start`
