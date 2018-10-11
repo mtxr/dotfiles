8df67be9accff34b7ff13a48a641b48a836422c2
Matheus Teixeira <matheus.mtxr@gmail.com>
2018-10-11 00:32:20 -0300

    Thu Oct 11 00:32:18 -03 2018

--- dotfiles/zsh/bootstrap.zsh
+++ dotfiles/zsh/bootstrap.zsh
@@ -18,26 +18,21 @@ export VSCODE_CLI=$(cat $VSCODE_FILE)
 
 HISTFILE="$HOME/.zsh_history"
 HISTSIZE=10000
-SAVEHIST=$HISTSIZE
+SAVEHIST=$(($HISTSIZE * 1.5))
 WORKSTATION_AUTOUPDATE=true
 
 setopt prompt_subst # Prompt Substitution
 setopt autocd # Set automatic cd (typing directory name with no 'cd')
 setopt interactivecomments # Enable interactive comments (# on the command line)
 # Nicer history
-setopt appendhistory
-setopt incappendhistory
-setopt extendedhistory
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
-setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
-setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
-setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
-setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
-setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt hist_ignore_all_dups
+setopt inc_append_history     # add commands to HISTFILE in order of execution
+setopt share_history          # share command history data
 
 # Initialize completion
 autoload -Uz compinit
