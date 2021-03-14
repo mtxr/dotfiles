bc233913b5b268e602673aab0a2cf978408c1e91
Matheus Teixeira <matheus.mtxr@gmail.com>
2021-03-14 12:34:56 -0300

    chore

--- run_once_10.install_system.zsh
+++ run_once_10.install_system.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 
+# all systems
+
 if [[ ! -d "$N_PREFIX" ]]; then
   echo 'Will install N, the node version manager...' && \
   eval $WEB_INSTALLER https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -y -n || echo "##### Failed to install 'N'."
