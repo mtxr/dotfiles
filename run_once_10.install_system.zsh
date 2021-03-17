3baafb01d3834ddf5fcec1376a399813aebaf3a7
Matheus Teixeira <matheus.mtxr@gmail.com>
2021-03-16 22:10:29 -0300

    add yarn to run once installs

--- run_once_10.install_system.zsh
+++ run_once_10.install_system.zsh
@@ -17,16 +17,17 @@ export N_PREFIX=${N_PREFIX:-"$HOME/.n"}
 if [[ ! -d "$N_PREFIX" ]]; then
   echo 'Will install N, the node version manager...' && \
   eval $WEB_INSTALLER https://git.io/n-install | bash -s -- -y -n || (echo "##### Failed to install 'N'." && exit 1)
-  zsh -c "npm install -g yarn"
 fi
 
 TO_INSTALL=""
-type diff-so-fancy &> /dev/null || TO_INSTALL="$TO_INSTALL diff-so-fancy"
+type yarn &> /dev/null || TO_INSTALL="yarn $TO_INSTALL"
+type diff-so-fancy &> /dev/null || TO_INSTALL="diff-so-fancy $TO_INSTALL"
 
 if [ "$TO_INSTALL" != "" ]; then
   echo "Installing '$TO_INSTALL'..."
   zsh -c "npm install -g $TO_INSTALL" > /dev/null || (echo "##### Failed to install '$TO_INSTALL'" && exit 1)
 fi
+
 
 echo "Installing GO apps"
 go get -u github.com/augustoroman/highlight
