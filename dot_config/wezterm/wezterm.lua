8413a5b73b2417c7d68051530544ff7786693473
Matheus Teixeira <matheus@mteixeira.dev>
2026-02-06 02:25:01 -0300

    wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -12,7 +12,28 @@ config.keys = {
     action = wezterm.action.ShowLauncherArgs {
       flags = 'FUZZY|DOMAINS',
     },
-  }
+  },
+  {
+    key = 's',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ShowLauncherArgs {
+      flags = 'FUZZY|DOMAINS',
+    },
+  },
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = wezterm.action.ShowLauncherArgs {
+      flags = 'FUZZY|TABS|LAUNCH_MENU_ITEMS|DOMAINS|WORKSPACES|COMMANDS',
+    }
+  },
+  {
+    key = 'k',
+    mods = 'ALT',
+    action = wezterm.action.ShowLauncherArgs {
+      flags = 'FUZZY|TABS|LAUNCH_MENU_ITEMS|DOMAINS|WORKSPACES|COMMANDS',
+    }
+  },
 }
 
 return config
